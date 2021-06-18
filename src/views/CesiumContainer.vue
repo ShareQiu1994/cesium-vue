f77b74b887991ef0d79975e94d37ac41b81ac130
刘博方 <lbf@ilng.cc>
2021-06-18 09:00:11 +0800

    1.使用vue3语法 2.添加vscode语法自动检查 3.添加eslint-babel

--- src/views/CesiumContainer.vue
+++ src/views/CesiumContainer.vue
@@ -2,23 +2,26 @@
  * @version: 1.0.0
  * @Author: liubofang<421419567@qq.com>
  * @Date: 2021-06-15 15:08:22
- * @LastEditTime: 2021-06-17 11:42:46
+ * @LastEditTime: 2021-06-18 08:55:24
 -->
 <template>
   <div id="cesiumContainer"></div>
 </template>
 
 <script>
-import "cesium/Build/Cesium/Widgets/widgets.css";
-import { Viewer } from "cesium";
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+import { onMounted } from 'vue'
+import { Viewer } from 'cesium'
 
 export default {
-  name: "CesiumContainer",
-  mounted() {
-    /* eslint no-new: */
-    new Viewer("cesiumContainer");
-  },
-};
+  name: 'CesiumContainer',
+  setup() {
+    onMounted(() => {
+      /* eslint no-new: */
+      new Viewer('cesiumContainer')
+    })
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
